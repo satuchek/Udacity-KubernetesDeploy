5b497da88d589db929f42a7162f0a1f56eac1611
satuchek <satuchek@gmail.com>
2021-09-22 12:38:23 -0400

    Added tests to build & failed test to check

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy & Insecure (like me)")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
--- test_main.py
+++ test_main.py
@@ -24,8 +24,9 @@ def client():
 
 def test_health(client):
     response = client.get('/')
+    assert False
     assert response.status_code == 200
-    assert response.json == 'Healthy and Insecure (like me)'
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
